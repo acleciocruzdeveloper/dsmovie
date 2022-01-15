45a6dc7182bdb4ba9f1837e48f9caf1bf8dc48cc
Aclecio Cruz <aclecioscruz@gmail.com>
2022-01-14 22:32:46 -0300

    Pagination layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,12 @@ import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from "components/NavBar";
 
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
