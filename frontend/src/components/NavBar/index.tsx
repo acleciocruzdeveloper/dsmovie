a7da429330dd7473ae0275ac9109d43cc628ff56
Aclecio Cruz <aclecioscruz@gmail.com>
2022-01-14 21:54:53 -0300

    NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './style.css';
+
+function NavBar() {
+    return (
+    <header>
+        <nav className='container'>
+            <div className='dsmovie-nav-content'>
+                <h1>DSMovie</h1>
+                <a href="https://github.com/acleciocruzdeveloper/dsmovie">
+                    <div className='dsmovie-contact-container'>
+                        <GithubIcon />
+                        <p className='dsmovie-contact-link'>/acleciocruzdeveloper</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>)
+}
+export default NavBar;
