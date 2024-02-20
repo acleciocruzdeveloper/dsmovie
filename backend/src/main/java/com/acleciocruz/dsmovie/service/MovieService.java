a782810be345ca135081f2382985d0db747f02de
aclecio cruz <aclecioscruz@gmail.com>
2024-02-20 09:57:20 -0300

    feature: inseri comentario author na service

--- backend/src/main/java/com/acleciocruz/dsmovie/service/MovieService.java
+++ backend/src/main/java/com/acleciocruz/dsmovie/service/MovieService.java
@@ -11,6 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Api NetHero
+ * @author Aclecio Cruz
+ */
 @Service
 public class MovieService {
 
